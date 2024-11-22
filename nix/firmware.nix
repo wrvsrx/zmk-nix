e609fbb9283e0b09c7d063e1a7ac2b31ada5ba48
wrvsrx <wrvsrx@outlook.com>
2024-11-22 14:44:24 +0800

    add my config

--- nix/firmware.nix
+++ nix/firmware.nix
@@ -8,15 +8,9 @@ buildSplitKeyboard {
 
   src = lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
 
-  board = "nice_nano_v2";
-  shield = "sofle_%PART% nice_view_adapter nice_view";
-
-  zephyrDepsHash = "sha256-H/ZlKyRiEZmDkVE3SSVcFsEF9tQ7XOCgbQ2tPpO2Pys=";
-
-  meta = with lib; {
-    description = "Keyboard firmware for Sofle RGB with nice!view screens";
-    license = licenses.mit;
-    platforms = platforms.all;
-    maintainers = with maintainers; [ /*lilyinstarlight*/ ];
-  };
+  board = "sofle_%PART%";
+  
+  shield = "nice_view_adapter nice_view";
+  
+  zephyrDepsHash = "sha256-AEB4VXd2Q+XWXxVN0753VXoqc0c4Irgcy8A4dpksZ8o=";
 }
